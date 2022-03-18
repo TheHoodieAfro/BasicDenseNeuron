4019626527f5e34998f87d00c4ae9f9733ac9cbd
Cristian Sanchez <crisey937@gmail.com>
2022-03-18 09:24:46 -0500

    Added activation function, weight init function

--- BDN.py
+++ BDN.py
@@ -1,3 +1,25 @@
 import numpy as np
+import random as rng
 
-print('lol')
+#Inicializa los pesos con valores random
+def __initializeWeights(size):
+
+    w = np.array(rng.sample(range(-10, 10), size))
+
+    return w
+
+#Funcion de inicializacion
+def __RELU(z):
+	return max(0.0, z)
+
+#Neurona
+def BDN(x):
+
+    w = __initializeWeights(len(x))
+    b = 0
+
+    z = w*x+b
+
+    y = __RELU(z)
+
+    print(y)
---
+++ tests.py
@@ -0,0 +1,8 @@
+"""
+Esta clase esta solo para probar la neurona
+"""
+
+import numpy as np
+import BDN
+
+print(BDN.BDN(np.array([1, 2, 3, 4, 5])))
